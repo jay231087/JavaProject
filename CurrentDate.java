94b74d1a0f0ff0c77a36bb61adc8b739291ed3ac
Mritunjay <jay231087@users.noreply.github.com>
2016-04-07 10:39:41 +0530

    Delete CurrentDate.java

--- CurrentDate.java
+++
@@ -1,7 +0,0 @@
-
-public class CurrentDate {
-  public static void main(String[] args) {
-    java.sql.Date date = new java.sql.Date(new java.util.Date().getTime());
-    System.out.println(date);
-  }
-}
